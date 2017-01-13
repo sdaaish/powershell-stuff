bc260632ade77525333cec371d98bf3f48b019d4
Stig Dahl <stig@charlottendal.net>
2017-01-13 15:48:08 +0100

    A working profile for powershell.

--- profile.ps1
+++ profile.ps1
@@ -8,22 +8,46 @@
 	- More info https://technet.microsoft.com/en-us/library/2008.10.windowspowershell.aspx
 	- And of course http://ss64.com/ps/syntax-profile.html
 	- https://www.interworks.com/blog/jpoehls/2011/03/25/scripting-tips-take-your-powershell-profile-everywhere-dropbox
+
 	2017-01-12/SDAA
 #>
 
-Write-Output Hello
-set-location c:
-$Shell.WindowTitle = "SysadminGeek"
-$Shell = $Host.UI.RawUI
-$size = $Shell.WindowSize
-$size.width=120
-$size.height=50
-$Shell.WindowSize = $size
-$size = $Shell.BufferSize
-$size.width=120
-$size.height=5000
-$Shell.BufferSize = $size
-$shell.BackgroundColor = "Gray"
-$shell.ForegroundColor = "Black"
-new-item alias:np -value C:\Windows\System32\notepad.exe
-#Clear-Host
+# Standard home dir
+Set-Location C:\
+
+# Get info about current user
+$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$name = ($id).Name
+$p = New-Object System.Security.Principal.WindowsPrincipal($id)
+if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $Host.UI.RawUI.WindowTitle = "Administrator: " + $Host.UI.RawUI.WindowTitle
+}
+else {
+    $Host.UI.RawUI.WindowTitle = "Powershell " + ($name)
+}
+
+# Window and buffersize
+$Host.UI.RawUI.WindowSize.width=120
+$Host.UI.RawUI.WindowSize.height=70
+$Host.UI.RawUI.BufferSize.width=120
+$Host.UI.RawUI.BufferSize.height=5000
+
+# Color
+if ($host.UI.RawUI.WindowTitle -match "Administrator") {
+    # Admin colors
+    $host.UI.RawUI.BackgroundColor = "Darkred"
+    $host.UI.RawUI.ForegroundColor = "White"
+}
+else {
+    # Normal colors
+    $Host.UI.RawUI.BackgroundColor = "Gray"
+    $Host.UI.RawUI.ForegroundColor = "Black"
+}
+
+#Aliases
+New-Item alias:np -value C:\Windows\System32\notepad.exe
+#New-Item alias:x -value "exit"
+
+# Clear terminal
+Clear-Host
+Write-Output "Hello ($name)!"
