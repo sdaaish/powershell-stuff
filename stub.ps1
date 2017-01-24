f51b7dd6f1c337debec8a38b27a626c6095c0bb1
Stig Dahl <stig@charlottendal.net>
2017-01-24 11:07:34 +0100

    Initial add2

---
+++ stub.ps1
@@ -0,0 +1,20 @@
+<#
+
+	.SYNOPSIS
+	This is a simple Powershell script to explain how to create help
+
+	.DESCRIPTION
+	The script itself will only print 'Hello World'. But that's cool. It's main objective is to show off the cool help thingy anyway.
+
+	.EXAMPLE
+	./HelloWorld.ps1
+
+	.NOTES
+	Put some notes here.
+
+	.LINK
+	http://kevinpelgrims.wordpress.com
+
+#>
+
+Write-Host ‘Hello World!’
