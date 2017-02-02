fed8942a5af3d5399919bc7f735c4832277e80f2
Stig Dahl <Stig@charlottendal.net>
2017-02-02 22:29:57 +0100

    Moved alias to separate file

--- profile.ps1
+++ profile.ps1
@@ -33,35 +33,3 @@ if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) 
 else {
     $Host.UI.RawUI.WindowTitle = "Powershell " + ($name)
 }
-
-#Aliases
-#New-Alias -Name np -Value C:\Windows\System32\notepad.exe
-#New-Item alias:x -value "exit"
-Set-Alias -Name src -Value reload-powershell-profile
-Set-Alias -Name alias -Value Get-Alias
-
-#Functions
-function cdh {
-    Set-Location $Env:UserProfile\
-}
-function cdr {
-    Set-Location $Env:UserProfile\Repos
-}
-function cdw {
-    Set-Location $Env:UserProfile\Downloads
-}
-function cdv {
-    Set-Location $Env:UserProfile\Vagrantdir
-}
-function reload-powershell-profile {
-	. $profile.CurrentUserAllHosts
-}
-function show-profiles {
-    $profile|Get-Member -MemberType NoteProperty
-}
-function show-colors {
-    [enum]::GetValues([System.ConsoleColor]) | Foreach-Object {Write-Host $_ -ForegroundColor $_}
-}
-function show-path {
-    $env:Path.split(";")
-}
