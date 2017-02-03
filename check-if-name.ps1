b95ee8e65b0436bd991f9e80adac5ecdcbca1c74
Stig Dahl <stig@charlottendal.net>
2017-02-03 15:50:31 +0100

    Changed methid for getting hostname and domain. Gets the primary domain-name and not only interface domain names.

--- check-if-name.ps1
+++ check-if-name.ps1
@@ -17,16 +17,21 @@
 	
 #>
 
-$hostname = ($env:ComputerName + "." + $env:UserDnsDomain).ToLower()
+#$hostname = ($env:ComputerName + "." + $env:UserDnsDomain).ToLower()
+$hostname = (Get-CIMInstance CIM_ComputerSystem).Name.tolower()
+$primarydomain = (Get-CIMInstance CIM_ComputerSystem).Domain.tolower()
 $interfaces = (Get-NetAdapter| select Name,ifIndex,Status| where Status -eq Up)
-Write-Output "Local hostname =`t`t$hostname`n"
+
+Write-Output "Local hostname =`t$hostname"
+Write-Output "Primary domain =`t$primarydomain`n"
 
 # Get interfaces that are up and print there ip-address
 foreach  ($if in $interfaces) {
     $ipv4 = (Get-NetIPAddress -ifIndex ($if).ifIndex -Type Unicast -AddressFamily IPv4).IPAddress
     $ifName = ($if).Name
     $ifIndex = ($if).ifIndex
-    Write-Output "Interface $ifName ($ifIndex) has address =`t$ipv4"
+    #Get-DnsClient -InterfaceIndex $ifIndex| select ConnectionSpecificSuffix
+        Write-Output "Interface $ifName ($ifIndex) has address =`t$ipv4"
 
     # Check for the reverse recordin DNS
     $dnshost = (Resolve-DnsName -Name $ipv4 -DnsOnly -ErrorAction Ignore).NameHost #Reverselookup
