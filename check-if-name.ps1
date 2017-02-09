261fd62d9874a4df51ffc1142ad37bc0a2d6a9e1
Stig Dahl <Stig@charlottendal.net>
2017-02-09 21:06:29 +0100

    Added handling of multiple ip's on forward lookup, and som text formatting

--- check-if-name.ps1
+++ check-if-name.ps1
@@ -13,6 +13,7 @@
 	.NOTES
 	Results differ depending if computer is AD-connected, have several active interfaces and have ipv6-addresses.
 	Prints the Primary-domain suffix and Primary-NVdomain suffix, just in case....
+	Domain is the current domain, NV-domain is one the after reboot.
 
 	Needs some better errorhandling
 	Some dead code currently in there.....
@@ -30,10 +31,10 @@ $interfaces = (Get-NetAdapter| select Name,ifIndex,Status| where Status -eq Up)
 $domain= (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\").Domain
 $nvdomain = (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\")."NV Domain"
 
-"{0,-20}{1,-30}" -f "Local hostname    = ","$hostname"
-"{0,-20}{1,-30}" -f "Primary AD-domain = ","$primarydomain"
-"{0,-20}{1,-30}" -f "Primary domain suffix = ","$domain"
-"{0,-20}{1,-30}" -f "Primary nvdomain suffix= ","$nvdomain`n"
+"{0,-20}{1,-30}" -f "Local hostname          = ","$hostname"
+"{0,-20}{1,-30}" -f "Primary AD-domain       = ","$primarydomain"
+"{0,-20}{1,-30}" -f "Primary domain suffix   = ","$domain"
+"{0,-20}{1,-30}" -f "Primary nvdomain suffix = ","$nvdomain`n"
 
 # Get interfaces that are up and print there ip-address
 foreach  ($if in $interfaces) {
@@ -45,31 +46,34 @@ foreach  ($if in $interfaces) {
 
     # If there is a domain suffix for interface, print it.
     if ($conspecsuffix) {
-	"{0,-49}{1,-35}" -f "Conn. specific suffix for $ifName ($ifIndex)  = ","$conspecsuffix"
+	"{0,-54}{1,-35}" -f "Conn. specific suffix for $ifName ($ifIndex)  = ","$conspecsuffix"
     }
-    "{0,-49}{1,-15}" -f "Interface $ifName ($ifIndex) has ipv4-address = ","$ipv4"
+    "{0,-54}{1,-15}" -f "Interface $ifName ($ifIndex) has ipv4-address = ","$ipv4"
     # Write every ipv6 address for the interface on a separate line
     foreach ($addr_6 in $ipv6) {
-	"{0,-49}{1,-30}" -f "Interface $ifName ($ifIndex) has ipv6-address = ","$addr_6"
+	"{0,-54}{1,-30}" -f "Interface $ifName ($ifIndex) has ipv6-address = ","$addr_6"
     }
 
     # Check for the reverse record in DNS
     $dnshost = (Resolve-DnsName -Name $ipv4 -DnsOnly -ErrorAction Ignore).NameHost #Reverselookup
     if ($dnshost) {
 	$dnshost= $dnshost.ToLower()
-	"{0,-49}{1,20}" -f "Reverse DNS lookup for $ipv4 = ","$dnshost"
+	"{0,-54}{1,-40}" -f "Reverse DNS lookup for $ipv4  = ","$dnshost"
 
 	# In case there is no name for dnshost
 	try {
-	    $revname = (Resolve-DnsName -Name $dnshost -DnsOnly -ErrorAction Stop).IpAddress #Forward Lookup
+	    $faddr = (Resolve-DnsName -Name $dnshost -DnsOnly -ErrorAction Stop).IpAddress #Forward Lookup
 	}
+	# This part is probably dead
 	catch {
 	    Write-Output "No name for $ipv4"
 	    Break
 	}
-	# Currently dead code...
+	# For every ip-address in the forward lookup, resolve to a name (reverse lookup)
 	finally {
-	    "{0,-40}{1,15}" -f "Forward DNS lookup for $dnshost =", "$revname`n"
+	    foreach ($name in $faddr) {
+		"{0,-40}{1,-40}" -f "Forward DNS lookup for $dnshost = ", "$name"
+	    }
 	}
     }
     else {
