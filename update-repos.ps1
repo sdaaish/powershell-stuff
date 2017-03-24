6d09aeed5db91eb3f354002db544e03ac5f43c3b
Stig Dahl <stig@charlottendal.net>
2017-03-24 15:50:54 +0100

    Detects hidden and not hidden directories.

--- update-repos.ps1
+++ update-repos.ps1
@@ -14,12 +14,14 @@
         git must be in the current PATH.
 
 	.LINK
-
+        About Get-ChildItem:
+        https://msdn.microsoft.com/en-us/powershell/reference/5.1/microsoft.powershell.core/providers/filesystem-provider/get-childitem-for-filesystem
+        https://msdn.microsoft.com/powershell/reference/5.1/Microsoft.PowerShell.Management/Get-ChildItem
 #>
 
 $repodir = $env:Userprofile + "\Repos"
 
-[Array] $gitdirs = (Get-ChildItem -Path $repodir -Name .git -Recurse -Directory -Attributes h)
+[Array] $gitdirs = (Get-ChildItem -Path $repodir -Name .git -Recurse -Directory -Attributes Hidden, !Hidden)
 
 foreach($dir in $gitdirs){
     $cdir = (Split-Path (${repodir} + "\" + ${dir}))
