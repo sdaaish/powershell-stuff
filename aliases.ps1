b4856063e67b02da4d5ade1a8f2f11613d196361
Stig Dahl <Stig@charlottendal.net>
2017-07-04 23:33:44 +0200

    Added alias for Connection profiles.

--- aliases.ps1
+++ aliases.ps1
@@ -14,8 +14,9 @@ Set-Alias -Name alias -Value Get-Alias
 Set-Alias -Name upr -Value ~\Repos\powershell-stuff\update-repos.ps1
 Set-Alias -Name ups -Value ~\Repos\powershell-stuff\update-status.ps1
 Set-Alias -Name em -Value emacs-client
-
 Set-Alias -Name emacs -Value emacs-client
+
+Set-Alias -name gnc -Valeu Get-NetConnectionProfile
 
 #Functions
 function .. {
