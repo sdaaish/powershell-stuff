0c16ddf7113647151be921d6a626b006c3f283f0
Stig Dahl <stig@charlottendal.net>
2017-07-06 16:28:47 +0200

    Added headers.

--- get-fav.ps1
+++ get-fav.ps1
@@ -1,3 +1,20 @@
+<#
+
+	.SYNOPSIS
+
+	.DESCRIPTION
+
+	.EXAMPLE
+
+	.NOTES
+	Put some notes here.
+
+	.LINK
+	https://github.com/sdaaish/powershell-stuff
+
+	From Technet: https://msdn.microsoft.com/en-us/powershell/reference/5.1/microsoft.powershell.core/about/about_comment_based_help
+#>
+
 param (
     [string] $web = $null
 )
