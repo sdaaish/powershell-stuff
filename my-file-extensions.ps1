225947c62a3b4a4a07a7df45b37c2a65b2dc5c80
Stig Dahl <Stig@charlottendal.net>
2017-09-24 21:02:37 +0200

    Added more extensions.

--- my-file-extensions.ps1
+++ my-file-extensions.ps1
@@ -1,5 +1,5 @@
 [string]$emacs = $env:UserProfile + "\bin\emx.cmd"
-[string[]] $fileexts = @("txtfile", "xmlfile", "Microsoft.PowerShellScript.1", "Microsoft.PowerShellData.1", "Microsoft.PowerShellModule.1")
+[string[]] $fileexts = @("txtfile", "xmlfile", "Microsoft.PowerShellScript.1", "Microsoft.PowerShellData.1", "Microsoft.PowerShellModule.1", "inffile","inifile","scriptletfile","Windows.CompositeFont","textfile")
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
   [Security.Principal.WindowsBuiltInRole] "Administrator"))
@@ -14,6 +14,6 @@ if ( -not (Test-Path $emacs)){
 }
 foreach ($extension in $fileexts)
 {
-    Write-Host "$extension"
+    Write-Host "Changing $extension"
     cmd /c "ftype $extension=`"$emacs`" `"%1`""
 }
