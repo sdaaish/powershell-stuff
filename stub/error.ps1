c7502aef908cffc0ae00c0bf480e0347210a434f
Stig Dahl <stig@charlottendal.net>
2017-12-01 12:21:29 +0100

    Placeholders for info.

---
+++ stub/error.ps1
@@ -0,0 +1,4 @@
+<#
+  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_redirection?view=powershell-5.1
+  https://blogs.msdn.microsoft.com/kebab/2013/06/09/an-introduction-to-error-handling-in-powershell/
+#>
