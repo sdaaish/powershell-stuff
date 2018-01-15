639f18b1860c76b33a3bca81afd703fcfadda669
Stig Dahl <stig@charlottendal.net>
2018-01-15 15:33:31 +0100

    Only show repos that are changed or have unstaged files.

--- update-status.ps1
+++ update-status.ps1
@@ -1,7 +1,7 @@
 <#
 
 	.SYNOPSIS
-	This script looks for modified files in local reposotories
+	This script looks for modified files in local repositories
 
 	.DESCRIPTION
 	The script searches for directories under %UserProfile%\Repos with a .git directory and makes a git status.
@@ -13,6 +13,8 @@
 	The location of repositories is in $env:UserProfile\Repos.
   Git must be in the current PATH.
 
+  Could be more generic regarding input directory
+
 	.LINK
 
 #>
@@ -23,6 +25,17 @@ $repodir = $env:Userprofile + "\Repos"
 
 foreach($dir in $gitdirs){
     $cdir = (Split-Path (${repodir} + "\" + ${dir}))
-    "`nChecking ${cdir}"
-    cmd /c "git -C ${cdir} status -sb"
+    $gitstatus=(git -C ${cdir} status --porcelain)
+
+    # Check if there were any modified files
+    if ($gitstatus|where {$_ -match '^\?\?|^A|^M|^R'}) {
+        Write-Host "`nRepo " -Foregroundcolor Green -NoNewLine
+        Write-Host "${cdir} "  -Foregroundcolor Cyan -NoNewLine
+        Write-Host "modified:"  -Foregroundcolor Green 
+
+        # Print the modified files
+        foreach($file in $gitstatus){
+            Write-Host "${file}" -Foregroundcolor Red
+        }
+    }
 }
