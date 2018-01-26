32972dd0510ec7cc2072a53d70296fd0351ef330
Stig Dahl <stig@charlottendal.net>
2018-01-26 18:07:22 +0100

    Added proxyfunctions
    Scripts to enable and disable proxysettings. Possible to enable with
    or without Autoconfigurl.

---
+++ Disable-InternetProxy.ps1
@@ -0,0 +1,29 @@
+﻿<#
+  .Synopsis
+Disables any active proxy
+
+.Description
+Removes proxysettings from the registry
+
+.Example
+
+#>
+[CmdletBinding()]
+param()
+
+Begin
+{
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+}
+
+Process
+{
+    Set-ItemProperty -path $regKey ProxyEnable -value 0
+    Set-ItemProperty -path $regKey ProxyServer -value ""
+    Set-ItemProperty -path $regKey AutoConfigURL -Value ""
+}
+
+End
+{
+    Write-Output "Proxy is now disabled"
+}
