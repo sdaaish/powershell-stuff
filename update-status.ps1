ce60463d2cb66951f835a63789e029729327d1bc
Stig Dahl <stig@charlottendal.net>
2018-02-11 20:44:32 +0100

    Fixed conversion of path
    Converts the path to an absolute value to handle relative paths better.

--- update-status.ps1
+++ update-status.ps1
@@ -28,7 +28,7 @@ Function check-git {
     [Array] $gitdirs = (Get-ChildItem -Path $repodir -Name .git -Recurse -Directory -Attributes Hidden, !Hidden)
 
     foreach($dir in $gitdirs){
-        $cdir = (Split-Path (${repodir} + "\" + ${dir}))
+        $cdir = (Convert-Path (Split-Path (${repodir} + "\" + ${dir})))
         $gitstatus=(git -C ${cdir} status --porcelain)
 
         # Check if there were any modified files
