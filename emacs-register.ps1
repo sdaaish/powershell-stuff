1e191e0146b9cda852c21423b1696d417e2e16fa
Stig Dahl <SDAA@svenskaspel.se>
2018-02-21 17:18:46 +0100

    Make this check for admin rights

--- emacs-register.ps1
+++ emacs-register.ps1
@@ -1,31 +1,62 @@
 <#
-	.SYNOPSIS
-	Script that adds "Emacs" as option in explorer "Open" contextual menu
+.SYNOPSIS
+Script that adds "Emacs" as option in explorer "Open" contextual menu
 
-	.DESCRIPTION
-	Requires Chocolatay and that emacs64 is installed. Otherwise change the URL for $emacs.
+.DESCRIPTION
+Requires that emacs64 is installed and the command-file emx.cmd, from https://raw.githubusercontent.com/sdaaish/winstuff/master/emx.cmd.
 
-	.NOTES
-	Problems and registry:
-	http://stackoverflow.com/questions/29267307/set-registry-key-to-open-notepad
-
-	Good Howto:
-	https://en.wikiversity.org/wiki/Windows_PowerShell/Registry
-#>	
+.NOTES
+This script starts emacsclientw and emacs as server plus opens the file given as argument.
+I found this the best way to open a file with emacs at the moment.
+#>
 
 # HKEY_CLASSES_ROOT is equal to HKLM:\Software\Classes
 $registryPath = 'HKLM:\Software\Classes\*\Shell\Emacs\Command'
-$emacs = "C:\ProgramData\chocolatey\bin\emacsclientw.exe -n -a `"`" `"%1`""
+$mycmd = "emx.cmd"
 
-if (!(Test-Path -LiteralPath $registryPath)) {
-    New-Item -Path $registryPath -Force |Out-Null
-    New-ItemProperty -LiteralPath $registryPath -Name "(default)" -value $emacs -PropertyType string |OUT-Null
+# Test for admin privs
+function Test-Admin
+{
+    $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
+    $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $prp.IsInRole($adm)
+}
+
+# Find the command-script
+function find-command {
+    try  {
+        $exe = (Get-Command -ErrorAction Stop $mycmd).source
+    }
+    catch {
+        Write-Host -ForeGroundColor Red "Could not find emx.cmd"
+        break
+    }
+    # Set the value to register for emacsclient and parameters
+    $exe = "$exe `"%1`""
+    return $exe
+}
+
+#Main
+$admincheck = Test-Admin
+if ( $admincheck ){
+
+    $emacs = find-command
+
+    if (!(Test-Path -LiteralPath $registryPath)) {
+        New-Item -Path $registryPath -Force |Out-Null
+        New-ItemProperty -LiteralPath $registryPath -Name "(default)" -value $emacs -PropertyType string |OUT-Null
+    }
+    else {
+        Set-ItemProperty -LiteralPath $registryPath -Name "(default)" -value $emacs -Force|OUT-Null
+    }
+
+    # Read the actual value
+    Write-Host -ForeGroundColor Yellow "New value for $registryPath.default: " -nonewline
+    $var = (Get-ItemProperty -LiteralPath $registryPath).'(default)'
+    Write-Host $var -ForeGroundColor DarkMagenta
 }
 else {
-    Set-ItemProperty -LiteralPath $registryPath -Name "(default)" -value $emacs -Force|OUT-Null
+    Write-Host -ForeGroundColor Red "You dont have administrative rights to change this!"
 }
 
-# Read the actual value
-Write-Host "New value for $registryPath.default is:"
-$var = (Get-ItemProperty -LiteralPath $registryPath).'(default)'
-Write-Host $var -ForeGroundColor DarkMagenta
