e3610ea66129d806915e59fc0115e1a816f423c7
Stig Dahl <stig@charlottendal.net>
2018-10-12 15:34:35 +0200

    Add StackOverflow for Powershell

--- aliases.ps1
+++ aliases.ps1
@@ -460,6 +460,20 @@ Function Search-EmacsSX {
     Start-Process $uri
 }
 
+# Search StackOverFlow for Powershell stuff
+Function Search-PowershellSX {
+    $search += $args -join "+"
+
+    if($search){
+        $uri = "https://stackoverflow.com/search?q=%5Bpowershell%5D+" + "$search"
+    }
+    else {
+        $uri = "https://stackoverflow.com/search?q=%5Bpowershell%5D+"
+    }
+
+    Start-Process $uri
+}
+
 # Start hugo locally and fire up a webpage
 Function test-hugo {
     Start-Job -ScriptBlock {& hugo server -D --disableFastRender}
