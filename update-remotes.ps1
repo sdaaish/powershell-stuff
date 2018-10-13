cd8df27346087ba11b9a8533c5b1bec1150e4c5d
Stig Dahl <stig@charlottendal.net>
2018-10-13 23:09:51 +0200

    Initial add

---
+++ update-remotes.ps1
@@ -0,0 +1,55 @@
+<#
+
+.SYNOPSIS
+This checks remote git repositories for uopdates.
+
+.DESCRIPTION
+The script searches for directories with a .git directory and makes a "git remote -v".
+
+.EXAMPLE
+./update-remotes.ps1
+./update-remotes.ps1 Myfolder
+./update-remotes.ps1 -Folder ~/repos
+
+.NOTES
+"git" must be in the current PATH.
+It won't update files that belong to lxss, ie Windows Linux Subshell (WSL), which is a bad idea.
+
+.LINK
+#>
+
+
+param (
+    [string]$folder = "."
+)
+
+Function check-git {
+    [Array] $gitdirs = (Get-ChildItem -Path $folder -Name .git -Recurse -Directory -Attributes Hidden, !Hidden)
+
+    foreach($dir in $gitdirs){
+
+        # Add .git to the path and convert to absolute
+        $cdir = (Convert-Path ((Split-Path (${folder} + "\" + ${dir}))))
+        # Trim trailing slash
+        $cdir = $cdir.Trim('\')
+
+        #If it is a WSL directory, lxss, dont update since bad things happens.
+        if ($cdir -match "AppData\\local\\lxss" ) {
+            Write-Host "Will not update git in $cdir`n" -ForegroundColor red
+        }
+        else {
+            "Checking ${cdir}"
+            Invoke-Expression 'git -C $cdir remote -v update'
+            "Done checking ${cdir}`n"
+        }
+    }
+}
+
+# Main
+if (Test-Path -Path $folder -PathType Container){
+    check-git
+    }
+else {
+    "$folder is not a valid directory. Exiting."
+    break
+}
