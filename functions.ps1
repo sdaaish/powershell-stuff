0be398b0eb652bffa2b19f97546ca56714517b52
Stig Dahl <stig@charlottendal.net>
2018-10-17 16:53:06 +0200

    Add function for censys

--- functions.ps1
+++ functions.ps1
@@ -25,3 +25,25 @@ Function mitmproxy {
     #docker run --rm -it -p 8080:8080 -p 127.0.0.1:8082:8081 mitmproxy/mitmproxy mitmweb --web-iface 0.0.0.0
 }
 
+# Censys domain lookup
+Function Search-Censys {
+    param (
+        $domain,
+        $apifile = "~\.settings\censys.api"
+    )
+
+    try {
+        $censys = Get-Content $apifile -ErrorAction Stop| ConvertFrom-Json
+    }
+    catch [System.IO.DirectoryNotFoundException],[System.IO.FileNotFoundException] {
+        Write-Error "No such file, $apifile" -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Some error occured, $apifile" -ErrorAction Stop
+    }
+
+    $id = $censys.CENSYS_API_ID
+    $secret = $censys.CENSYS_API_SECRET
+
+    docker run --rm -e CENSYS_API_SECRET=$secret -e CENSYS_API_ID=$id --name censys sdaaish/censys_subdomain_finder $domain
+}
