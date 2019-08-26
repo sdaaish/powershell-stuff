0fd66b5387356c0408948c4362de11911c2e6257
Stig Dahl <stig@charlottendal.net>
2019-08-26 23:10:27 +0200

    Change structure

--- install-mymodules.ps1
+++ install-mymodules.ps1
@@ -1,11 +1,27 @@
-# Install modules
+# Install my preferred modules
 Function Install-MyModules {
     [cmdletbinding()]
     param()
-    Install-Module -Name Get-ChildItemColor -Scope CurrentUser
-    Install-Module -Name PSReadline -Scope CurrentUser
-    Install-Module -Name Posh-Git -Scope CurrentUser -AllowPrerelease -Force
-    Install-Module -Name BuildHelpers -Scope CurrentUser
-    Install-Module -Name PSScaffold -Scope CurrentUser
-    Install-Module -Name Posh-Docker -Scope CurrentUser
+
+    $StableModules = @(
+        "Get-ChildItemColor"
+        "PSReadline"
+        "BuildHelpers"
+        "PSScaffold"
+        "Posh-Docker"
+    )
+
+    $BetaModules = @(
+        "Posh-Git"
+    )
+
+    foreach ($module in $StableModules){
+        Write-Host "Installing module $module"
+        Install-Module -Name $module -Scope CurrentUser
+    }
+
+    foreach ($module in $BetaModules){
+        Write-Host "Installing module $module"
+        Install-Module -Name $module -Scope CurrentUser -AllowPrerelease -Force
+    }
 }
