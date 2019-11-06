ef0dbbf454f9c63531ec476bbd735804d3e7cfcc
Stig Dahl <stig@charlottendal.net>
2019-11-06 13:24:58 +0100

    Add ignore cert function

--- aliases.ps1
+++ aliases.ps1
@@ -908,3 +908,27 @@ Function Get-CommandSyntax {
     )
     Get-Command $command -Syntax
 }
+
+Function Ignore-SelfSignedCerts {
+    try {
+        Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
+        Add-Type -TypeDefinition  @"
+          using System.Net;
+          using System.Security.Cryptography.X509Certificates;
+          public class TrustAllCertsPolicy : ICertificatePolicy {
+              public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,WebRequest request, int certificateProblem) {
+                  return true
+                }
+          }
+"@
+
+        Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
+    }
+
+    catch {
+        Write-Host $_ -ForegroundColor "Yellow"
+    }
+
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
