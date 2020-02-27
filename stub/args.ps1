8725a67522190176ab811586f25226e79a749a0d
Stig Dahl <stig@charlottendal.net>
2020-02-27 22:26:09 +0100

    Remove stub

--- stub/args.ps1
+++
@@ -1,8 +0,0 @@
-# See http://edgylogic.com/blog/powershell-and-external-commands-done-right/ for more info about handling arguments
-if ($args.count -eq 0 ) {
-    Write-Host "No args"
-}
-else {
-    Write-Host "This is the args $args"
-}
-
