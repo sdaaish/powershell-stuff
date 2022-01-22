2a373d8ee60e9ef75119b566b7e88a2bcc97584b
Stig Dahl <stig@charlottendal.net>
2022-01-22 22:41:17 +0100

    Let top run continous

--- aliases.ps1
+++ aliases.ps1
@@ -593,11 +593,15 @@ Function top {
         "Memory"  {$sort = "TotalMemory (M)"}
         default  {$sort = "CPU"}
     }
-    Get-Process|
+    while ($true){
+        Clear-Host
+        Get-Process|
       Select-Object -Property Id, ProcessName,
     @{name="CPU";expression={($_.CPU).ToString("#") -as [int]}},
     @{name="TotalMemory (M)";expression={($_.VM + $_.PM) / 1MB -as [int]}},
-    Handles| Sort-Object -Property $sort -Descending -Top 20|Format-Table -Auto
+        Handles| Sort-Object -Property $sort -Descending -Top 20|Format-Table -Auto
+        Start-Sleep 5
+    }
 }
 
 # Show filehash for all executables running
